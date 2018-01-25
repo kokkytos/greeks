020da523798a4f90824d43e10d7fa886e4ae30db
leonidas <leonidas@liakos@yahoo.gr>
2018-01-25 19:26:47 +0200

    Som labels moved to external file (data/settings.js). Rem

---
+++ data/settings.js
@@ -0,0 +1,18 @@
+var period={
+    minYear: 1950,
+    maxYear: 1975,
+}
+
+ var labels = {
+    pois: "Places",
+    grayscale: "Grayscale",
+    streets: "Streets",
+    datayear: "Data for year: ",
+    dataperiod:"Data for period: ",
+    points: "Points:" ,
+    popName: "Name",
+    popDateSt:"Date Start",
+    popDateEnd: "Date End",
+    popAddress: "Address",
+    
+};
